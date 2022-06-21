0373f5758f7583001881abbda1cf950b58a229ad
ooitanojohn <qmkh733310@gmail.com>
2022-06-21 21:43:54 +0900

    new tab以外は対応した

---
+++ readme.md
@@ -0,0 +1,12 @@
+# したいこと 検索結果から特定の文字を含むものを除外したい
+
+## [get_started](https://developer.chrome.com/docs/extensions/mv3/getstarted/)
+
+## background
+- [インストール時に実行させておきたい処理がある場合](https://developer.chrome.com/docs/extensions/mv3/getstarted/#background-script)
+
+## permissions
+- ストレージ
+
+## action
+- タブに出てくる奴
--- src/popup.js
+++ src/popup.js
@@ -1,14 +1,21 @@
 
 'use strict';
 let submit = document.getElementsByTagName('form')[0];
-console.log(submit);
 
 if ((typeof submit !== 'undefined')) { // formが取得できていれば実行
   submit.addEventListener('click', () => {
-    let searchWord = document.getElementsByClassName('gLFyf')[0].value; // 検索ワード
+    let gLFyf = document.getElementsByClassName('gLFyf'); // 検索フォームのclassで絞り込み
+    let children = '';
+    let searchWord = '';
+    for (children of gLFyf) {
+      if (children.tagName === 'INPUT') {
+        searchWord = children // inputタグの値を代入
+      }
+    }
     let removeWord = "jQuery";
-    if (!searchWord.includes(removeWord)) { // 検索外ワードがフォーム内にあるか
-      document.getElementsByClassName('gLFyf')[0].value = searchWord + ' -' + removeWord; // なければ検索対象外ワードを検索フォームに代入
+    if (!searchWord.value.includes(removeWord)) { // 検索外ワードがフォーム内にあるか
+      searchWord.value = searchWord.value + ' -' + removeWord; // なければ検索対象外ワードを検索フォームに代入
+      console.log('add')
     }
   })
 }
