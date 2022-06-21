6a1224144f0d6d158156ccd376d47764eec1f4be
ooitanojohn <qmkh733310@gmail.com>
2022-06-22 00:11:37 +0900

    [add]localストレージにブロックワードリスト追加

--- src/popup.js
+++ src/popup.js
@@ -1,22 +1,13 @@
-
-'use strict';
-let submit = document.getElementsByTagName('form')[0];
-
-if ((typeof submit !== 'undefined')) { // formが取得できていれば実行
-  submit.addEventListener('click', () => {
-    let gLFyf = document.getElementsByClassName('gLFyf'); // 検索フォームのclassで絞り込み
-    let children = '';
-    let searchWord = '';
-    for (children of gLFyf) {
-      if (children.tagName === 'INPUT') {
-        searchWord = children // inputタグの値を代入
-      }
-    }
-    let removeWord = "jQuery";
-    if (!searchWord.value.includes(removeWord)) { // 検索外ワードがフォーム内にあるか
-      searchWord.value = searchWord.value + ' -' + removeWord; // なければ検索対象外ワードを検索フォームに代入
-      console.log('add')
-    }
-  })
+const button = document.getElementsByTagName('button')[0];
+const input = document.getElementById('addWord');
+let words = [];
+if ((typeof button !== 'undefined')) {
+  button.addEventListener('click', () => {
+    words.push(input.value);
+    chrome.storage.local.set({ 'blockList': words }); // chrome storageに保存
+    chrome.storage.local.get('blockList', (result) => { //値取得
+      console.log(result)
+    });
+  });
 }
 
